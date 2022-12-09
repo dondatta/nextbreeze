6a65de4c1b47ac55b7e5b9e0d5aae4384b95ba75
dondatta <chris011972@gmail.com>
2022-12-08 19:54:13 -0500

    yes

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
         },
         ecmaVersion: 2020,
     },
-    plugins: ['react', '@next/eslint-plugin-next', 'prettier'],
+    plugins: ['react',  'prettier'],
     rules: {
         'import/prefer-default-export': 0,
         'no-console': 'warn',
